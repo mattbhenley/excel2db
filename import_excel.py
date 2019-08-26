21126ae8c47e09b6be59eacf93aca11aa423349e
Matt Henley <mattbhenley@gmail.com>
2019-08-26 18:39:24 -0500

    added connection code

--- import_excel.py
+++ import_excel.py
@@ -11,3 +11,15 @@ username = ''
 password = ''
 # Provide the file name. The complete path is required. Leave the r behind the ' as it is need to successfully upload.
 filename = r'/Users/matthenley/Desktop/Sample_excel.xlsx'
+
+# User will get an errpr if unable to connect to the database. 
+try:
+    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server +
+                          ';DATABASE=' + database + ';UID=' + username + ';Trusted_Connection=yes;PWD=' + password)
+except:
+    try:
+        cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' +
+                              server+';DATABASE='+database+';UID='+username+';PWD=' + password)
+    except Exception as e:
+        print(e)
+        exit(0)
